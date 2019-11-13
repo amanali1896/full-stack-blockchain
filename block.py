e15b3824c60c2f8699849efd5affd21a7bc77e2b
amanali1896 <pamanalionline@gmail.com>
2019-11-13 18:01:53 -0500

    pycharm imported

--- block.py
+++ block.py
@@ -1,13 +1,14 @@
-#created a new file because when we modularize the program, we can make it 
+# created a new file because when we modularize the program, we can make it
 # work better
 
 class Block:
     """
     block - unit of storage
     """
-    def __init__(self, data): #block class is where data is added
+
+    def __init__(self, data):  # block class is where data is added
         self.data = data
 
-    def __repr__(self): #another dunder method to override the implementation of
-        #standard printing of object address
+    def __repr__(self):  # another dunder method to override the implementation of
+        # standard printing of object address
         return f'Block - Data: {self.data}'
--- blockchain.py
+++ blockchain.py
@@ -1,32 +1,35 @@
-from block import Block #importing from block.py
-#import line evaluates the entire from that we import from
-#so if we print anything in Block, it gets printed here too. 
+from block import Block  # importing from block.py
+
+
+# import line evaluates the entire from that we import from
+# so if we print anything in Block, it gets printed here too.
 class Blockchain:
     """
     implemented as a list of blocks
     - datasets of transactions
 
     """
+
     def __init__(self):
         self.chain = []
-    
+
     def add_block(self, data):
-        self.chain.append(Block(data)) # we append the block to the list
-    
+        self.chain.append(Block(data))  # we append the block to the list
+
     def __repr__(self):
         return f'Blockchain : {self.chain}'
 
 
-
-def main(): 
-    #whatever you write here will be executed if this file is the main method
-    #otherwise it will not be
+def main():
 
 
-def if __name__ == '__main__':
-     # this method will be executed
-    #only if this file is a main module.
-    #now main module will be found out by the file that is 
-    #currently being executes
-    main()
-    
+# whatever you write here will be executed if this file is the main method
+# otherwise it will not be
+
+
+if __name__ == '__main__':
+# this method will be executed
+# only if this file is a main module.
+# now main module will be found out by the file that is
+# currently being executes
+main()
