9e37a85c1bc32e9e47ee0d8794315868dfb2d41c
amanali1896 <pamanalionline@gmail.com>
2019-11-13 18:09:26 -0500

    Update block.py

--- block.py
+++ block.py
@@ -9,7 +9,7 @@ class Block:
     def __init__(self, timestamp, last_hash, hash, data):  # block class is where data is added
         self.timestamp = timestamp
         self.last_hash = last_hash
-        self.hash = hash  # data structure of the blockchain. 
+        self.hash = hash  # data structure of the blockchain.
         self.data = data
 
     def __repr__(self):  # another dunder method to override the implementation of
